7544211a5bf83660f506b6a67496ccdf52fa6edc
Yogesh <yogdeep.k@gmail.com>
2019-02-05 11:38:51 +0530

    Two directive created..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { FormComponent } from './form/form.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight-directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { DirectivesComponent } from './directives/directives.component';
     ServerComponent,
     ServersComponent,
     FormComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
